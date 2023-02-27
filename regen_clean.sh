a1bb4ab89c0ebbd1a35c0d84df510161812c1034
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2023-02-27 18:55:13 -0500

    feat: add bash script for running all regen jobs, add to ci
    Signed-off-by: Alexander Skvortsov <sasha.skvortsov109@gmail.com>

---
+++ regen_clean.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+dune build
+
+cli="./_build/default/infra/bin/main.exe"
+
+$cli save-diffs todo_list
+$cli reset-workbench todo_list
+
+#cli save-diffs snake_game
+#cli reset-workbench snake_game
