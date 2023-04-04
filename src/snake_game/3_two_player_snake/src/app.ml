9c8a00a6c3a949197ab33275f0c02f1af999e355
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2023-04-04 12:04:57 -0400

    feat: extract chain_incr_effects out into a lib
    It's a bit advanced to  introduce in the tutorial, I might talk a bit about it at the end. Ideally, it would be upstreamed into `bonsai_extra`.
    
    Signed-off-by: Alexander Skvortsov <sasha.skvortsov109@gmail.com>

--- src/snake_game/3_two_player_snake/src/app.ml
+++ src/snake_game/3_two_player_snake/src/app.ml
@@ -22,34 +22,6 @@ let get_keydown_key evt =
   |> Js_of_ocaml.Js.to_string
 ;;
 
-let chain_scheduler
-  : type a. a Value.t -> ((a -> unit Ui_effect.t) list -> unit Ui_effect.t) Computation.t
-  =
- fun input ->
-  let module Action = struct
-    type t = Run of (a -> unit Effect.t) list [@@deriving sexp]
-  end
-  in
-  let apply_action ~inject ~schedule_event input _model (Action.Run effect_fns) =
-    match effect_fns with
-    | effect_fn :: dependents ->
-      schedule_event (Effect.Many [ effect_fn input; inject (Action.Run dependents) ])
-    | [] -> ()
-  in
-  let open Bonsai.Let_syntax in
-  let%sub (), inject =
-    Bonsai.state_machine1
-      [%here]
-      (module Unit)
-      (module Action)
-      ~default_model:()
-      ~apply_action
-      input
-  in
-  let%arr inject = inject in
-  fun effects -> inject (Action.Run effects)
-;;
-
 let component =
   let open Bonsai.Let_syntax in
   (* State *)
@@ -66,7 +38,7 @@ let component =
     ; apples = Apple_state.Model.apples [ apple1; apple2 ]
     }
   in
-  let%sub scheduler = chain_scheduler game_elements in
+  let%sub scheduler = Chain_incr_effects.component game_elements in
   (* Tick logic *)
   let%sub () =
     let%sub clock_effect =
